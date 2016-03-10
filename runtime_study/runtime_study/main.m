c0214ce0abec41aab1f649ca51266eda316d2c91
babywolf <591138406@qq.com>
2016-03-10 15:57:33 +0800

    init project

---
+++ runtime_study/runtime_study/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  runtime_study
+//
+//  Created by babywolf on 16/3/10.
+//  Copyright © 2016年 com.babywolf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
